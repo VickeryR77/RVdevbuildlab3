cb8c5f9602f01cf841d587a799757ca7f5206581
Rick Vickery <rickvickery@quickenloans.com>
2020-07-09 15:53:27 -0400

    Updating code to include extended challenges.

--- Regexp/Program.cs
+++ Regexp/Program.cs
@@ -15,39 +15,38 @@ namespace Regexp
         public static void Build()
         {
             //Build Regex Requirements.
+            string nameReg = @"^[A-Z][a-z]{0,30}"; 
+            //Format: XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
-            string nameReg = @"^[A-Z][a-z]{0,30}";
-            string emailReg = @".{4,30}[^!@#$%^&*()<>;:'.,/{}~+\-_=|]@.{4,10}[^!@#$%^&*()<>;:'.,/{}~+\-_=|]\..{1,3}[^!@#$%^&*()<>;:'.,/{}~+\-_=|]";
-            string phoneNumberReg = @"[0-9]{2,3}\-[0-9]{2,3}\-[0-9]{3,4}"; //XXX-XXX-XXXX
-            string phone2NumberReg = @"^\([0-9]{3}\)-[0-9]{3}-[0-9]{4}"; //(XXX)-XXX-XXXX
-            string phone3NumberReg = @"[0-9]{3}\.[0-9]{3}\.[0-9]{4}"; //XXX.XXX.XXXX
-            string dateReg = @"\d\d\/\d\d\/\d\d\d\d"; //XX/XX/XXXX
+            string emailReg = @"[A-Za-z0-9]{5,30}\@[A-Za-z0-9]{5,10}\.[A-Za-z0-9]{2,3}"; 
+            //Format: XXXXX-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX@XXXXX-XXXXXXXXXX.XX-XXX
 
-            //Idea for date problem
-            //3 separate variables determined by Console.ReadLine(); with max criteria.
-            //1+2+3.ToString
-            //Regex evaluate the string.
+            string phoneNumberReg = @"[0-9]{3}\-[0-9]{3}\-[0-9]{4}"; 
+            //Format: XXX-XXX-XXXX
 
-            //Look up HTML code to put into this.
+            string phone2NumberReg = @"^\([0-9]{3}\)-[0-9]{3}-[0-9]{4}"; 
+            //Format: (XXX)-XXX-XXXX
 
+            string phone3NumberReg = @"[0-9]{3}\.[0-9]{3}\.[0-9]{4}"; 
+            //Format: XXX.XXX.XXXX
+
+            string dateReg = @"\d\d\/\d\d\/\d\d\d\d"; 
+            //Format: XX/XX/XXXX
 
             //Run Regex Methods.
             GetValidName(nameReg);
             GetValidEmail(emailReg);
             GetValidPhone(phoneNumberReg, phone2NumberReg, phone3NumberReg);
             GetValidDate(dateReg);
-
         }
-        public static void GetValidName(string req)
+        public static void GetValidName(string req) //Method for testing user input against Regex requirements
         {
             Regex reg = new Regex(req);
             Console.WriteLine("Please tell us your name.");
             Console.WriteLine("A maximum of 30 characters, only alphabetical characters please.");
             Console.WriteLine("");
-
             string mystring = Console.ReadLine();
             bool matched = reg.IsMatch(mystring);
-
             Console.WriteLine(matched);
         }
 
@@ -59,7 +58,6 @@ namespace Regexp
             Console.WriteLine("");
             string mystring = Console.ReadLine();
             bool matched = reg.IsMatch(mystring);
-
             Console.WriteLine(matched);
         }
 
@@ -67,10 +65,9 @@ namespace Regexp
         {
             Regex reg = new Regex(req);
             Console.WriteLine("Please enter your desired phone number.");
-            Console.WriteLine("Format rules: XXX-XXX-XXXX");
+            Console.WriteLine("Format rules: XXX-XXX-XXXX, XXX.XXX.XXXX, (XXX)-XXX-XXXX");
             string mystring = Console.ReadLine();
             bool matched = reg.IsMatch(mystring);
-            
             if (matched == true)
             {
                 Console.WriteLine(matched);
@@ -85,7 +82,6 @@ namespace Regexp
         {
             Regex reg = new Regex(req2);
             bool matched = reg.IsMatch(mystring);
-
             if (matched == true)
             {
                 Console.WriteLine(matched);
@@ -103,45 +99,78 @@ namespace Regexp
             bool matched = reg.IsMatch(mystring);
             Console.WriteLine(matched);
             return;
-            
         }
 
         public static void GetValidDate(string req)
         {
-            Regex reg = new Regex(req);
-            Console.WriteLine("Please enter your desired date.");
-            Console.WriteLine("Format rules: mm/dd/yyyy");
-            string mystring = Console.ReadLine();
-            bool matched = reg.IsMatch(mystring);
+            Regex reg = new Regex(req); 
+            int monthNumber = GetValidMonth("Please enter your month. (1-12)");
+            GetValidDateFormat(monthNumber,out string monthNum);
 
+            int dayNumber = GetValidDay("Please enter your day number. (1-31)");
+            GetValidDateFormat(dayNumber,out string dayNum);
+
+            int yearNumber = GetValidYear("Please enter your year number. (1000-9999)");
+            string yearNum = yearNumber.ToString();
+
+            Console.WriteLine("");
+            Console.WriteLine("Checking against valid format: mm / dd / yyyy");
+            string mystring = (monthNum+"/"+dayNum+"/"+yearNum);
+            Console.WriteLine(mystring);
+            bool matched = reg.IsMatch(mystring);
             Console.WriteLine(matched);
         }
-    }
-}
 
-
-/*
- * if (matched == true)
+        public static int GetValidMonth(string prompt)
+        {
+            Console.WriteLine(prompt);
+            bool isValid = int.TryParse(Console.ReadLine(), out int number);
+            while (!isValid || number <= 0 || number > 12)
             {
-                Console.WriteLine(matched);
-                return;
+                Console.WriteLine("Please enter a valid number between 1 & 12.");
+                isValid = int.TryParse(Console.ReadLine(), out number);
+            }
+            return number;
+        }
+
+        public static int GetValidDay(string prompt)
+        {
+            Console.WriteLine(prompt);
+            bool isValid = int.TryParse(Console.ReadLine(), out int number);
+
+            while (!isValid || number <= 0 || number > 31)
+            {
+                Console.WriteLine("Please enter a valid number between 1 & 31.");
+                isValid = int.TryParse(Console.ReadLine(), out number);
+            }
+            return number;
+        }
+
+        public static int GetValidYear(string prompt)
+        {
+            Console.WriteLine(prompt);
+            bool isValid = int.TryParse(Console.ReadLine(), out int number);
+
+            while (!isValid || number <= 1000 || number > 9999)
+            {
+                Console.WriteLine("Please enter a valid number between 1000 & 9999.");
+                isValid = int.TryParse(Console.ReadLine(), out number);
+            }
+            return number;
+        }
+
+        public static bool GetValidDateFormat(int num1, out string s1)
+        {
+            if (num1 < 10)
+            {
+                s1 = ("0" + num1.ToString());
+                return false;
             }
             else
             {
-                Regex reg2 = new Regex(req2);
-bool matched2 = reg.IsMatch(mystring);
-                if(matched2 == true)
-                {
-                    Console.WriteLine(matched2);
-                    return;
-                }
-                else
-                {
-                    Regex reg3 = new Regex(req3);
-bool matched3 = reg.IsMatch(mystring);
-Console.WriteLine(matched3);
-                    return;
-                    
-                }
+                s1 = (num1.ToString());
+                return true;
             }
-    */
+        }
+    }
+}
